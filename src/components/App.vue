b3b1faf056a17b46aaf9b7d8741ea02e1e75a469
Peter Butkovic <butkovic@gmail.com>
2021-05-07 23:03:11 +0200

    feat: basic tiles defined

--- src/components/App.vue
+++ src/components/App.vue
@@ -5,8 +5,9 @@
       color="primary"
       dark
     >
+
       <div class="d-flex align-center">
-        <v-img
+        <!-- <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
@@ -22,35 +23,37 @@
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        /> -->
+        Podiyumm
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://podiymm.github.io"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Podiyumm</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
+
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <LandingTiles/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import LandingTiles from './LandingTiles';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    LandingTiles,
   },
 
   data: () => ({
---
+++ src/components/LandingTiles.vue
@@ -0,0 +1,90 @@
+<template>
+
+<v-card
+    max-width="400"
+    class="mx-auto"
+  >
+    <!-- <v-system-bar
+      color="pink darken-2"
+      dark
+    >
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-window-minimize</v-icon>
+
+      <v-icon>mdi-window-maximize</v-icon>
+
+      <v-icon>mdi-close</v-icon>
+    </v-system-bar> -->
+
+    <!-- <v-app-bar
+      dark
+      color="pink"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>My Music</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar> -->
+
+    <v-container>
+      <!-- <v-row dense>
+        <v-col cols="12"> -->
+          <v-card
+          >
+<v-img :src="require('./../assets/iconfinder_projector_6137765.png')"
+transition="scale-transition"
+          width="300"/>
+            <v-card-actions>
+              <v-btn text>
+                Projector
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          <v-card
+          >
+<v-img :src="require('./../assets/iconfinder_Music_7149758.png')"
+transition="scale-transition"
+          width="300" />
+            <v-card-actions>
+              <v-btn text>
+                Musician
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+
+          <v-card>
+<v-img :src="require('./../assets/iconfinder_373-Present_2124250.png')"
+transition="scale-transition"
+          width="300" />
+            <v-card-actions>
+              <v-btn text>
+                Presenter
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+
+      <v-card
+          >
+<v-img :src="require('./../assets/iconfinder_book_audio_add_103382.png')"
+transition="scale-transition"
+          width="300" />
+            <v-card-actions>
+              <v-btn text>
+                Songbook
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+
+
+
+    </v-container>
+  </v-card>
+
+
+</template>
