cd4889da702c9e0703b3d4dc4330c258b62d6c82
Peter Butkovic <butkovic@gmail.com>
2021-08-22 17:11:00 +0200

    fix: publicPath introduced (/landing-ui)

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  // https://cli.vuejs.org/config/#publicpath
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/landing-ui/' // prod
+      : '/', // dev
 }
